2ee5aace06e9e50d9f6d755bfc6f8b723429f895
Stefan Bodewig <stefan.bodewig@innoq.com>
2015-04-19 20:40:50 +0200

    add AssemblyProduct attributes - #8

--- src/main/net-constraints/AssemblyInfo.cs
+++ src/main/net-constraints/AssemblyInfo.cs
@@ -13,6 +13,8 @@
 */
 using System.Reflection;
 using System.Runtime.CompilerServices;
+
 [assembly: AssemblyTitleAttribute("XMLUnit Constraints for NUnit")]
 [assembly: AssemblyDescription("Constraints Classes of XMLUnit for .NET")]
 [assembly: InternalsVisibleTo("xmlunit-constraints.tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100fdec9d5858956ff10ff9cf39935441a7d4a216475a467e951ddd8fcce0b74443696680a6cfb58f8b1d96166acb76f9eae0431a223cecb30e224c21f30e57c457747f6ba176e6c22f48e105b200b458509b6de815cccdfc9ebe59ae4024c4b8659d2f2fcb65b1688f8327b10c96ed7e5f536ba6a1978ae86aca438439efbd6cb6")]
+[assembly: AssemblyProduct("XMLUnit.NET Constraints")]
