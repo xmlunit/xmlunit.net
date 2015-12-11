088471c3abf841042a3e771d9226f1d3791baec3
Stefan Bodewig <stefan.bodewig@freenet.de>
2015-12-11 16:41:42 +0100

    adapt the documentation build process

--- src/doc/monodoc/run-monodoc.sh
+++ src/doc/monodoc/run-monodoc.sh
@@ -10,14 +10,17 @@ OUT_DIR="$BUILD/doc"
 HTML_DIR="$BUILD/html"
 rm -rf "$OUT_DIR" "$HTML_DIR"
 mkdir -p "$OUT_DIR"
-cp -r "$MY_DIR/core" "$MY_DIR/constraints" "$OUT_DIR"
+cp -r "$MY_DIR/core" "$MY_DIR/constraints-nunit2" "$MY_DIR/constraints-nunit3" "$OUT_DIR"
 
 monodocer -pretty -importslashdoc:"$DEBUG_DIR/xmlunit-core.XML"\
           -assembly:"$DEBUG_DIR/xmlunit-core.dll"\
           -path:"$OUT_DIR/core"
-monodocer -pretty -importslashdoc:"$DEBUG_DIR/xmlunit-constraints.XML"\
-          -assembly:"$DEBUG_DIR/xmlunit-constraints.dll"\
-          -path:"$OUT_DIR/constraints"
+monodocer -pretty -importslashdoc:"$DEBUG_DIR/xmlunit-nunit3-constraints.XML"\
+          -assembly:"$DEBUG_DIR/xmlunit-nunit3-constraints.dll"\
+          -path:"$OUT_DIR/constraints-nunit3"
+monodocer -pretty -importslashdoc:"$DEBUG_DIR/xmlunit-nunit2-constraints.XML"\
+          -assembly:"$DEBUG_DIR/xmlunit-nunit2-constraints.dll"\
+          -path:"$OUT_DIR/constraints-nunit2"
 
-monodocs2html -out "$HTML_DIR" "$OUT_DIR/core" "$OUT_DIR/constraints"
+monodocs2html -out "$HTML_DIR" "$OUT_DIR/core" "$OUT_DIR/constraints-nunit3" "$OUT_DIR/constraints-nunit2"
 
