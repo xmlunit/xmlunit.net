c6a98c556141f5d71e214ea963ba1f8a5e1ed5bf
Stefan Bodewig <stefan.bodewig@freenet.de>
2016-03-26 06:51:50 +0100

    bump versions

--- src/shared/CommonAssemblyInfo.cs
+++ src/shared/CommonAssemblyInfo.cs
@@ -25,8 +25,8 @@ namespace Org.XmlUnit
 {
     internal static class XmlUnitVersion
     {
-        internal const string ApiVersion = "2.1.0";
+        internal const string ApiVersion = "2.1.1";
         internal const string AssemblyVersion = ApiVersion + ".80";
-        internal const string Version = ApiVersion + "";
+        internal const string Version = ApiVersion + "-alpha-01";
     } 
 }
