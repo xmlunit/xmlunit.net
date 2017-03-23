50f6cbafd1bddccbc5ff3cd075b812750c36e6a8
Stefan Bodewig <stefan.bodewig@freenet.de>
2017-03-23 20:36:17 +0100

    adapt to changed XML extension

--- src/doc/monodoc/run-monodoc.sh
+++ src/doc/monodoc/run-monodoc.sh
@@ -12,13 +12,13 @@ rm -rf "$OUT_DIR" "$HTML_DIR"
 mkdir -p "$OUT_DIR"
 cp -r "$MY_DIR/core" "$MY_DIR/constraints-nunit2" "$MY_DIR/constraints-nunit3" "$OUT_DIR"
 
-monodocer -pretty -importslashdoc:"$DEBUG_DIR/xmlunit-core.XML"\
+monodocer -pretty -importslashdoc:"$DEBUG_DIR/xmlunit-core.xml"\
           -assembly:"$DEBUG_DIR/xmlunit-core.dll"\
           -path:"$OUT_DIR/core"
-monodocer -pretty -importslashdoc:"$DEBUG_DIR/xmlunit-nunit3-constraints.XML"\
+monodocer -pretty -importslashdoc:"$DEBUG_DIR/xmlunit-nunit3-constraints.xml"\
           -assembly:"$DEBUG_DIR/xmlunit-nunit3-constraints.dll"\
           -path:"$OUT_DIR/constraints-nunit3"
-monodocer -pretty -importslashdoc:"$DEBUG_DIR/xmlunit-nunit2-constraints.XML"\
+monodocer -pretty -importslashdoc:"$DEBUG_DIR/xmlunit-nunit2-constraints.xml"\
           -assembly:"$DEBUG_DIR/xmlunit-nunit2-constraints.dll"\
           -path:"$OUT_DIR/constraints-nunit2"
 
