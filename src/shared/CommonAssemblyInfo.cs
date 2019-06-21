807726c752ea08454489394947b98fd19c7f9c1b
Stefan Bodewig <stefan.bodewig@freenet.de>
2019-06-21 20:34:16 +0200

    removed a line by accident

--- src/shared/CommonAssemblyInfo.cs
+++ src/shared/CommonAssemblyInfo.cs
@@ -26,6 +26,7 @@ namespace Org.XmlUnit
     internal static class XmlUnitVersion
     {
         internal const string ApiVersion = "2.7.1";
-        internal const string AssemblyVersion = ApiVersion + ".174";
+        internal const string AssemblyVersion = ApiVersion + ".175";
+        internal const string Version = ApiVersion;
     } 
 }
