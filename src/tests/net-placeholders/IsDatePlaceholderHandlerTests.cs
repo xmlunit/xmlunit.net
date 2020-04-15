98ff68d30339b8f735318c475449d06f50b2fbc3
Cameron Cowen <cameronc94@gmail.com>
2020-04-15 13:03:01 +0100

    Added tests for more date formats

--- src/tests/net-placeholders/IsDatePlaceholderHandlerTests.cs
+++ src/tests/net-placeholders/IsDatePlaceholderHandlerTests.cs
@@ -13,8 +13,11 @@ namespace Org.XmlUnit.Placeholder
         private IPlaceholderHandler placeholderHandler = new IsDatePlaceholderHandler();
 
         [Test()]
-        [TestCase("2020-01-01", TestName = "ShouldEvaluateDateWithDashes", ExpectedResult = ComparisonResult.EQUAL)]
+        [TestCase("01-01-2020", TestName = "ShouldEvaluateDateWithDashes", ExpectedResult = ComparisonResult.EQUAL)]
+        [TestCase("01/01/2020", TestName = "ShouldEvaluateDateWithDots", ExpectedResult = ComparisonResult.EQUAL)]
         [TestCase("01/01/2020", TestName = "ShouldEvaluateDateWithSlashes", ExpectedResult = ComparisonResult.EQUAL)]
+        [TestCase("2020-01-01T15:00", TestName = "ShouldEvaluateSortableDate", ExpectedResult = ComparisonResult.EQUAL)]
+        [TestCase("2020-01-01 15:00:00Z", TestName = "ShouldEvaluateUniversalDate", ExpectedResult = ComparisonResult.EQUAL)]
         [TestCase("19/06/2020", TestName = "ShouldEvaluateUKDate", ExpectedResult = ComparisonResult.EQUAL)]
         [TestCase("06/19/2020", TestName = "ShouldEvaluateUSDate", ExpectedResult = ComparisonResult.EQUAL)]
         [TestCase("01/01/2020 15:00", TestName = "ShouldEvaluateDateWithTime", ExpectedResult = ComparisonResult.EQUAL)]
