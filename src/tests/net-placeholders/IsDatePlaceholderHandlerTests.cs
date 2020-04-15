da610dfa64c41a29a352c00d85024c15754d1b05
Cameron Cowen <cameronc94@gmail.com>
2020-04-15 13:36:28 +0100

    Made separate tests for different cultures

--- src/tests/net-placeholders/IsDatePlaceholderHandlerTests.cs
+++ src/tests/net-placeholders/IsDatePlaceholderHandlerTests.cs
@@ -3,6 +3,7 @@ using Org.XmlUnit.Diff;
 using Org.XmlUnit.Placeholder;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 
 namespace Org.XmlUnit.Placeholder
@@ -12,20 +13,27 @@ namespace Org.XmlUnit.Placeholder
     {
         private IPlaceholderHandler placeholderHandler = new IsDatePlaceholderHandler();
 
-        [Test()]
+        [Test]
         [TestCase("01-01-2020", TestName = "ShouldEvaluateDateWithDashes", ExpectedResult = ComparisonResult.EQUAL)]
         [TestCase("01/01/2020", TestName = "ShouldEvaluateDateWithDots", ExpectedResult = ComparisonResult.EQUAL)]
         [TestCase("01/01/2020", TestName = "ShouldEvaluateDateWithSlashes", ExpectedResult = ComparisonResult.EQUAL)]
         [TestCase("2020-01-01T15:00", TestName = "ShouldEvaluateSortableDate", ExpectedResult = ComparisonResult.EQUAL)]
         [TestCase("2020-01-01 15:00:00Z", TestName = "ShouldEvaluateUniversalDate", ExpectedResult = ComparisonResult.EQUAL)]
-        [TestCase("19/06/2020", TestName = "ShouldEvaluateUKDate", ExpectedResult = ComparisonResult.EQUAL)]
-        [TestCase("06/19/2020", TestName = "ShouldEvaluateUSDate", ExpectedResult = ComparisonResult.EQUAL)]
         [TestCase("01/01/2020 15:00", TestName = "ShouldEvaluateDateWithTime", ExpectedResult = ComparisonResult.EQUAL)]
         [TestCase(null, TestName = "ShouldNotEvaluateNull", ExpectedResult = ComparisonResult.DIFFERENT)]
         [TestCase("", TestName = "ShouldNotEvaluateEmpty", ExpectedResult = ComparisonResult.DIFFERENT)]
         [TestCase("This is a test date 01/01/2020", TestName = "ShouldNotEvaluateStringContainingDate", ExpectedResult = ComparisonResult.DIFFERENT)]
         public ComparisonResult EvaluateTest(string testText)
         {
+            return placeholderHandler.Evaluate(testText);
+        }
+
+        [Test]
+        [TestCase("19/06/2020", "en-GB", TestName = "ShouldEvaluateUKDate", ExpectedResult = ComparisonResult.EQUAL)]
+        [TestCase("06/19/2020", "en-US", TestName = "ShouldEvaluateUSDate", ExpectedResult = ComparisonResult.EQUAL)]
+        public ComparisonResult EvaluateOtherCulturesTest(string testText, string cultureCode)
+        {
+            CultureInfo.CurrentCulture = new CultureInfo(cultureCode);
             return placeholderHandler.Evaluate(testText);
         }
 
